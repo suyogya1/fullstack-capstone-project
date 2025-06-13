e3f755aade49adb564e114aa481106d01c40f575
suyogya1 <suyogya227@gmail.com>
2025-06-13 07:29:24 -0400

    Module 2 completed

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -17,12 +17,14 @@ async function connectToDatabase() {
 
     // Task 1: Connect to MongoDB
     // {{insert code}}
+    await client.connect()
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
     //{{insert code}}
-
+    const dbInstance = client.db("giftsDB")
     // Task 3: Return database instance
     // {{insert code}}
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
